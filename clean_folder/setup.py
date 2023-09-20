ac5493631d07b1de251572b514b15a40b2962fa0
PSirgun <Sirgun28@gmail.com>
2023-09-20 11:40:27 +0300

    Змінив. В 7 дз не вимагали лізти скрипт.

--- clean_folder/setup.py
+++ clean_folder/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
 setup(name='clean_folder',
-      version='0.1.10',
+      version='0.1.11',
       entry_points = {'console_scripts':['clean_folder = clean_folder.clean:dz6']})
       
